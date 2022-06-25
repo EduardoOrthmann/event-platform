37766510210ea5e5bbfad73c97f5206831816eeb
Eduardo <orthmann2811@gmail.com>
2022-06-25 18:38:32 -0300

    bug: import classnames

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,7 +2,7 @@ import { format, isPast } from 'date-fns';
 import { CheckCircle, Lock } from 'phosphor-react';
 import ptBR from 'date-fns/locale/pt-BR';
 import { Link, useParams } from 'react-router-dom';
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 interface LessonProps {
 	title: string;
